37e12201c26649a138f23bc3554f161f02b770bb
LuciZR <151607296+LuciZR@users.noreply.github.com>
2024-11-12 19:10:41 +0530

    ZR

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
-  SESSION_NAME: process.env.SESSION_NAME || "Bot~"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://ZR:Luci@luncizr.xziz9in.mongodb.net/?retryWrites=true&w=majority&appName=LunciZR",
+  SESSION_NAME: process.env.SESSION_NAME || "ZR~"
 };
